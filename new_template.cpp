119d16549ceccee00007119fb15cb6a3f16058dd
Abhishek Yadav <abhishekcms611@gmail.com>
2018-12-31 17:18:50 +0530

    added template for num_theory

---
+++ new_template.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;  
+#define ll long long int
+#define uint unsigned int     
+#define read(n) scanf("%d",&n)
+#define readll(n) scanf("%lld",&n)
+#define read2(n,m) scanf("%d%d",&n,&m)
+#define read3(n,m,l) scanf("%d%d%d",&n,&m,&l)     
+#define fr(i,n)     for(int i=0;i<n;i++)
+#define frr(i,a,b)   for(int i=a;i<b;i++)
+#define rf(i,n)     for(int i=n-1;i>=0;i--)     
+#define init(mem,v) memset(mem,v,sizeof(mem))  
+#define sz(v) ((v).size())
+#define vc(r) vector< r >
+#define mk(a,b) make_pair(a,b)
+#define pb push_back
+#define all(v) (v).begin(),(v).end()
+#define tr(v,it) for(decltype((v).begin()) it = (v).begin(); it != (v).end() ; it++)
+#define vpresent(v,x) ((v).find(x) != (v).end())
+#define present(v,x) (find(all(v),x) != (v).end())  
+#define x first
+#define y second
+typedef pair<int,int> pii;
+typedef pair<int,char> pic;
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template <unsigned int N> class Field {
+    typedef unsigned int ui;
+    typedef unsigned long long ull;
+	inline ui pow(ui a, ui p){ui r=1,e=a;while(p){if(p&1){r=((ull)r*e)%N;}e=((ull)e*e)%N;p>>=1;}return r;}
+	inline ui inv(ui a){return pow(a,N-2);}
+public:
+    inline Field(int x = 0) : v(x) {}
+	inline Field<N> pow(int p){return (*this)^p; }
+	inline Field<N> operator^(int p){return {(int)pow(v,(ui)p)};}
+    inline Field<N>&operator+=(const Field<N>&o) {if (v+o.v >= N) v += o.v - N; else v += o.v; return *this; }
+    inline Field<N>&operator-=(const Field<N>&o) {if (v<o.v) v -= o.v-N; else v-=o.v; return *this; }
+    inline Field<N>&operator*=(const Field<N>&o) {v=(ull)v*o.v % N; return *this; }
+    inline Field<N>&operator/=(const Field<N>&o) { return *this*=inv(o.v); }
+    inline Field<N> operator+(const Field<N>&o) const {Field<N>r{*this};return r+=o;}
+    inline Field<N> operator-(const Field<N>&o) const {Field<N>r{*this};return r-=o;}
+    inline Field<N> operator*(const Field<N>&o) const {Field<N>r{*this};return r*=o;}
+    inline Field<N> operator/(const Field<N>&o) const {Field<N>r{*this};return r/=o;}
+    inline Field<N> operator-() {if(v) return {(int)(N-v)}; else return {0};};
+    inline Field<N>& operator++() { ++v; if (v==N) v=0; return *this; }
+    inline Field<N> operator++(int) { Field<N>r{*this}; ++*this; return r; }
+    inline Field<N>& operator--() { --v; if (v==-1) v=N-1; return *this; }
+    inline Field<N> operator--(int) { Field<N>r{*this}; --*this; return r; }
+    inline bool operator==(const Field<N>&o) const { return o.v==v; }
+	inline bool operator!=(const Field<N>&o) const { return o.v!=v; }
+	inline explicit operator ui() const { return v; }
+	inline static vector<Field<N>>fact(int t){vector<Field<N>>F(t+1,1);for(int i=2;i<=t;++i){F[i]=F[i-1]*i;}return F;}
+	inline static vector<Field<N>>invfact(int t){vector<Field<N>>F(t+1,1);Field<N> X{1};for(int i=2;i<=t;++i){X=X*i;}F[t]=1/X;for(int i=t-1;i>=2;--i){F[i]=F[i+1]*(i+1);}return F;}
+private: ui v;
+};
+template<unsigned int N>istream &operator>>(std::istream&is,Field<N>&f){unsigned int v;is>>v;f=v;return is;}
+template<unsigned int N>ostream &operator<<(std::ostream&os,const Field<N>&f){return os<<(unsigned int)f;}
+template<unsigned int N>Field<N> operator+(int i,const Field<N>&f){return Field<N>(i)+f;}
+template<unsigned int N>Field<N> operator-(int i,const Field<N>&f){return Field<N>(i)-f;}
+template<unsigned int N>Field<N> operator*(int i,const Field<N>&f){return Field<N>(i)*f;}
+template<unsigned int N>Field<N> operator/(int i,const Field<N>&f){return Field<N>(i)/f;}
+
+typedef Field<998244353> FF;
+
+int main(int argc, char* argv[]) {
+    int n; cin >> n;
+    auto F = FF::fact(n);
+    auto I = FF::invfact(n);
+}
+
